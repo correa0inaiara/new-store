ccdc58c40d901245578953f5071b412ba99fccc9
Naiara Correa <correa0inaiara@gmail.com>
2026-01-06 15:44:25 -0300

    carousel fix

--- src/app/components/recomendations.tsx
+++ src/app/components/recomendations.tsx
@@ -14,6 +14,7 @@ export const Recomendations = ({ products }: { products: Products }) => {
         slidesToShow: 4,
         slidesToScroll: 4,
         initialSlide: 0,
+        slider: 'div',
         responsive: [
             {
                 breakpoint: 1024,
@@ -46,39 +47,41 @@ export const Recomendations = ({ products }: { products: Products }) => {
 
     return (
 
-        <div className="pt-5 pb-5 slider-container custom-slider">
-            <Slider {...settings}>
-                {products.map((product, index) => (
-                    <div className="card bg-base-100 w-96 shadow-sm">
-                        <div className="m-auto w-50">
-                            <figure className="relative w-full h-full">
-                                <Image
-                                    src={product.image.src ?? produto_quebrado}
-                                    alt={product.image.alt ?? 'product'}
-                                    width={300}
-                                    height={180}
-                                    className="rounded-xl"
-                                />
-                            </figure>
-                        </div>
+        <main className="custom-slider">
+            <div className="pt-5 pb-5 slider-container">
+                <Slider {...settings}>
+                    {products.map((product, index) => (
+                        <div className="card bg-base-100 w-96 shadow-sm">
+                            <div className="m-auto w-50">
+                                <figure className="relative w-full h-full">
+                                    <Image
+                                        src={product.image.src ?? produto_quebrado}
+                                        alt={product.image.alt ?? 'product'}
+                                        width={300}
+                                        height={180}
+                                        className="rounded-xl"
+                                    />
+                                </figure>
+                            </div>
 
-                        <div className="card-body items-center">
-                            <h2 className="card-title">
-                                {product.title}
-                            </h2>
-                            <p>{product.description}</p>
-                            <div className="card-actions items-center justify-center gap-2">
-                                <span className="text-xl font-bold">${product.price}</span>
-                                <button className="btn btn-primary btn-sm">Buy Now</button>
-                            </div>
-                            <div className="card-actions justify-center gap-2">
-                                <div className="badge badge-outline">{product.category.title}</div>
-                                <div className="badge badge-outline">{product.subcategory.title}</div>
+                            <div className="card-body items-center">
+                                <h2 className="card-title">
+                                    {product.title}
+                                </h2>
+                                <p>{product.description}</p>
+                                <div className="card-actions items-center justify-center gap-2">
+                                    <span className="text-xl font-bold">${product.price}</span>
+                                    <button className="btn btn-primary btn-sm">Buy Now</button>
+                                </div>
+                                <div className="card-actions justify-center gap-2">
+                                    <div className="badge badge-outline">{product.category.title}</div>
+                                    <div className="badge badge-outline">{product.subcategory.title}</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </Slider>
-        </div>
+                    ))}
+                </Slider>
+            </div>
+        </main>
     )
 }
